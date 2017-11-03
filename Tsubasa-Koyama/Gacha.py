38e9a930d733f15b9e34ce8d829ca76aed33f5eb
KoyamaTsubasa <19990824tsubasa@gmailcom>
2017-11-04 02:14:48 +0900

    first commit

---
+++ Tsubasa-Koyama/Gacha.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy as sp
+import Gacha as G
+
+
+
+
+def mygacha(myP):
+    GachaResult = np.random.multinomial(1,myP)
+    GachaResult = np.where(GachaResult ==1)[0][0]
+    return GachaResult
+
+def gachaRepeat(numPull, myP):
+    record = np.zeros(numPull)
+    for k in range(numPull):
+        record[k] = mygacha(myP)
+
+    return record
+
+
+def myPull(select,myPs):
+    myval = G.mygacha(myPs[select])
+    return myval

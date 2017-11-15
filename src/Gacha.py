b6311850ef83b4fd86ea5d336f7f472cec809efb
masomatics <koyama.masanori@gmail.com>
2017-11-15 22:38:10 +0900

    created a functional Epsilon Greedy code

--- src/Gacha.py
+++ src/Gacha.py
@@ -8,7 +8,8 @@ import random
 
 def mygacha(myP):
     GachaResult = np.random.multinomial(1,myP)
-    return GachaResult[0]
+    GachaResult = np.where(GachaResult ==0)[0][0]
+    return GachaResult
 
 def gachaRepeat(numPull, myP):
     record = np.zeros(numPull)
@@ -29,6 +30,10 @@ def UpdateRecord(currentRecord, myPs, select):
     return currentRecord
 
 
+def KoyamaMachine():
+    pass
+
+
 def EpsilonGreedyMachine(currentRecord, myPs, epsilon = 0.2):
     Maxval = np.max(currentRecord)
     gachaIndex = range(len(myPs))
@@ -41,3 +46,39 @@ def EpsilonGreedyMachine(currentRecord, myPs, epsilon = 0.2):
         select = theBest
 
     return select
+
+
+def run_experiment(gachaPs,numExps,myEpsilon, mode = 'EG'):
+    numGacha = len(gachaPs)
+    myPs = np.zeros([numGacha ,2])
+
+
+    for k in range(numGacha):
+        myPs[k,0] = gachaPs[k]
+        myPs[k,1]= 1- gachaPs[k]
+
+    #This is for Debugging purpose!!!
+    #pdb.set_trace()
+
+    currentRecord = np.zeros(numGacha)
+    numLeverPulled = np.zeros(numGacha)
+
+    for j in range(numExps):
+        # Machine chooses the lever to pull
+        if mode == 'EG':
+            machineSelect = G.EpsilonGreedyMachine(currentRecord, myPs, epsilon = myEpsilon)
+        else:
+            raise NotImplementedError
+
+        #Update the record according to the decision
+        currentRecord = G.UpdateRecord(currentRecord, myPs, machineSelect)
+        numLeverPulled[machineSelect]+=1
+
+        if np.mod(j, 100) ==0 :
+        #Report!
+            print("%s th pull made"%j)
+            print("Number of Pulls:%s"%numLeverPulled)
+            print("Current Score:%s"%currentRecord)
+    FinalScore= np.sum(currentRecord)
+
+    return FinalScore
---
+++ src/experiment.py
@@ -0,0 +1,20 @@
+import numpy as np
+import Gacha as G
+import pdb
+
+def main():
+
+    numExps = 1000
+    myEpsilon = 0.4
+    gachaPs = np.array([0.3, 0.4, 0.8, 0.2, 0.9])
+
+
+    FinalScore = G.run_experiment(gachaPs,numExps,myEpsilon, mode = 'EG')
+
+    print("Final Score was %s"%(FinalScore))
+
+
+
+
+if __name__ == '__main__':
+    main()

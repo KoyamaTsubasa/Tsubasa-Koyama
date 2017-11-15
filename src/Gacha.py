1572f6341d71f8cf4eb594fbda1fcc2890b3ffc2
masomatics <koyama.masanori@gmail.com>
2017-11-16 00:30:22 +0900

    Wrote instructions and comments

--- src/Gacha.py
+++ src/Gacha.py
@@ -38,15 +38,26 @@ def UpdateRecord(currentRecord, myPs, select):
     return currentRecord
 
 
+'''
+Create your own Tsubasa Machine here!!!
+'''
+
 def TsubasaMachine():
+
     return 0
 
 
+'''
+Epsilon Greedy Machine will take a look at the currentRecord of each Gacha and decide which Gacha to pull in the next round.
+'''
 def EpsilonGreedyMachine(currentRecord, myPs, epsilon = 0.2):
+
+    #Find the Gacha with the best Score
     Maxval = np.max(currentRecord)
     gachaIndex = range(len(myPs))
     theBest = random.choice(np.where(currentRecord == Maxval)[0])
 
+    #If this is one, EXPLORE.  Otherwise, pick the best Gacha
     GreedyOrEpsilon = np.random.multinomial(1,[epsilon, 1-epsilon])[0]
     if GreedyOrEpsilon == 1:
         select = random.choice(gachaIndex)
@@ -55,6 +66,9 @@ def EpsilonGreedyMachine(currentRecord, myPs, epsilon = 0.2):
 
     return select
 
+'''
+run experiment will take in the information of Gacha, name of the machine, and run the experiment
+'''
 
 def run_experiment(gachaPs,numExps,myEpsilon, mode = 'EG'):
     numGacha = len(gachaPs)
@@ -73,36 +87,49 @@ def run_experiment(gachaPs,numExps,myEpsilon, mode = 'EG'):
     numLeverPulled = np.zeros(numGacha)
 
 
+    '''
+    Pull the Gacha for numExps number of times!
+    iteration is the number of pulls you have made so far.
+    '''
+    for iteration in range(numExps):
 
-    for j in range(numExps):
-        # Machine chooses the lever to pull
+        '''
+        Machine choose the lever here!! mode will decide the type of the machine!
+        '''
         if mode == 'EG':
+
+            '''
+            Epsilon Greedy Mode!
+            '''
             machineSelect = EpsilonGreedyMachine(currentRecord, myPs, epsilon = myEpsilon)
         elif mode =='Tsubasa':
+
+            '''
+            Tsubasa Machine MODE!!! You need to change HERE!!
+            '''
             machineSelect =  TsubasaMachine()
         else:
             raise NotImplementedError
 
+        '''
+        Pull the lever, and Update the record according to the decision
+        '''
         oldRecord = copy.copy(currentRecord)
-        #Update the record according to the decision
         currentRecord = G.UpdateRecord(currentRecord, myPs, machineSelect)
+        numLeverPulled[machineSelect]+=1
 
-        #History of outcomes Up to now
+
+        #History of outcomes Up to now! Use it at your own discretion
         #outcomeThisRound = currentRecord - oldRecord
         #RecordHistoryList = RecordHistoryList + [outcomeThisRound]
         #RecordHistory = np.array(RecordHistoryList)
 
 
-        #if j > 100:
-        #    pdb.set_trace()
-
-        numLeverPulled[machineSelect]+=1
-
-
-
-        if np.mod(j, 100) ==0 :
-        #Report!
-            print("%s th pull made"%j)
+        '''
+        Report the intermediate result at every 100 iterations
+        '''
+        if np.mod(iteration, 100) ==0 :
+            print("%s th pull made"%iteration)
             print("Number of Pulls:%s"%numLeverPulled)
             print("Current Score:%s"%currentRecord)
     FinalScore= np.sum(currentRecord)
--- src/experiment.py
+++ src/experiment.py
@@ -4,20 +4,36 @@ import pdb
 
 def main():
 
-    numExps = 1000
+    #Number of experiments
+
+    numExperiment = 500
+
+    #Number of pulls on each experiment
+    numIter = 500
     myEpsilon = 0.2
-    gachaPs = np.array([0.3, 0.4, 0.8, 0.2, 0.9])
+    gachaPs = np.array([0.5, 0.4, 0.2, 0.45, 0.99])
     mymode = 'EG'
 
     '''
-    If mode == 'EG',  This will run Epsilon Greedy.
-    If mode == 'Tsubasa', This will run YOURS
+    If mode = 'EG',  This will run Epsilon Greedy.
+    If mode = 'Tsubasa', This will run YOURS
     '''
 
-    FinalScore = G.run_experiment(gachaPs,numExps,myEpsilon, mode = mymode)
+    scores  = []
 
-    print("Final Score was %s"%(FinalScore))
+    for expIndx in range(numExperiment):
 
+        FinalScore = G.run_experiment(gachaPs,numIter,myEpsilon, mode = mymode)
+
+        scores += [FinalScore]
+        print("Final Score was %s"%(FinalScore))
+
+    '''
+    Mean is the average.
+    sqrt of variance is Standard Deviation
+    '''
+    print("average record of the final Scores:%s"%np.mean(scores))
+    print("std of the record of the final Scores:%s"%np.sqrt(np.var(scores)))
 
 
 
